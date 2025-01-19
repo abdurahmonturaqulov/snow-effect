104304650e1d20180ea4555d489b6a6b3bca16a4
Abdurahmon <rahmlow.1217@mail.ru>
2025-01-19 15:48:02 +0500

    add3foiles

--- js/app.js
+++ js/app.js
@@ -1,19 +1,17 @@
-setInterval(createSnow, 80);
-    
+setInterval(createSnow, 100)
 
+function createSnow() {
+  const snow = document.createElement('i')
+  snow.classList.add('fa')
+  snow.classList.add('fa-snowflake')
 
-function createSnow () {
-const snow = document.createElement("i");
-snow.classList.add("fa-solid");
-snow.classList.add("fa-snowflake");
+  snow.style.left = Math.random() * window.innerWidth + 'px'
+  snow.style.animationDirection = Math.random() * 3 + 2 + 's'
+  snow.style.opacity = Math.random()
+  document.body.append(snow)
+  snow.style.fontSize = Math.random * 10 + 10 + 'px'
 
-snow.style.left = Math.random() * window.innerWidth + "px";
-snow.style.animationDuration = Math.random() * 3 + 2 + "s";
-snow.style.opacity = Math.random();
-snow.style.fontSize = Math.random * 10 + 10 + "px";
-document.body.append(snow);
-
-setTimeout(() => {
-    snow.remove();
-}, 4000)
+  setTimeout(() => {
+    snow.remove()
+  }, 4000)
 }
